659df3f0c6ef13fcf20caf5a4dce59ff8d5c05dc
woodmawa <will.woodman@btinternet.com>
2019-10-07 13:12:14 +0100

    get time back from a generated sequence

--- src/main/groovy/sequence/testGenerator.groovy
+++ src/main/groovy/sequence/testGenerator.groovy
@@ -6,7 +6,7 @@ SequenceGenerator seq = new SequenceGenerator()
 
 long num1 = seq.nextId()
 
-long last = seq.lastTimestamp
+
 long num2 = seq.nextId()
 long num3 = seq.nextId()
 long num4 = seq.nextId()
@@ -15,6 +15,6 @@ long num6 = seq.nextId()
 
 println "seq : [$num1, $num2, $num3, $num4, $num5, $num6]"
 
-println seq.getDateStringForSequence(last)
+println seq.getDateStringForSequence(num1)
 
 println "exit"
