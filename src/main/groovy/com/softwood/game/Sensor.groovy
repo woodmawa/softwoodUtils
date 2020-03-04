036534ffd4f5e0324bf72f73380a28b42651eb45
woodmawa <will.woodman@btinternet.com>
2020-03-04 14:02:14 +0000

    used Optional<Rule> in Rules

--- src/main/groovy/com/softwood/game/Sensor.groovy
+++ src/main/groovy/com/softwood/game/Sensor.groovy
@@ -6,7 +6,7 @@ import org.codehaus.groovy.runtime.MethodClosure
 
 class Sensor {
     String name
-    Closure $detect = {key, attmap-> attmap.get(key)}
+    Closure $detect = {key, attmap -> attmap.get(key)}
 
     //try and sense state on arg - nominally player in this example
     Fact sense(arg) {
