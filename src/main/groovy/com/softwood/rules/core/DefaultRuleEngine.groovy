c04a9b89d0f250e00b6b93eb12a61063601d0238
woodmawa <will.woodman@btinternet.com>
2020-03-09 11:28:42 +0000

    bit of tidy up round listener handling in rulesEngine

--- src/main/groovy/com/softwood/rules/core/DefaultRuleEngine.groovy
+++ src/main/groovy/com/softwood/rules/core/DefaultRuleEngine.groovy
@@ -5,21 +5,37 @@ import com.softwood.rules.api.Rule
 import com.softwood.rules.api.RuleEngine
 import com.softwood.rules.api.RuleSet
 import groovy.transform.MapConstructor
+import groovy.util.logging.Slf4j
 
 /**
  * default Rule engine and takes all the bindable rulelisteners, ruleEngineListeners
  * from the abstract base class
  */
 
+@Slf4j
 class DefaultRuleEngine extends AbstractRuleEngine implements RuleEngine {
 
+    /**
+     *
+     * check the facts against each rule and return a collection pf boolean for each rule
+     * if There is any null from any listener then the rule processing should be stopped
+     * @param facts
+     * @param rules
+     * @param arg - any data item that you wish the executed rule to receive
+     * @return collection of true and false for each rule assesed against the facts
+     */
     Collection<Boolean> check(Facts facts, RuleSet rules, arg = null) {
         assert rules, facts
-        println "with rules $rules, size ${rules.size()}"
+        log.debug("with rules $rules, size ${rules.size()}")
+
         Collection<Boolean> results = rules.iterator().collect { Rule rule ->
             println "rule '$rule.name' evaluate facts $facts"
 
-            ruleListeners.each {it?.beforeEvaluate(rule, facts)}
+            def yehNey = ruleListeners.each {it?.beforeEvaluate(rule, facts)}
+            if (yehNey.any {it == false}) {
+                log.debug( "check facts with rules: One of the RuleListeners returned false, you should stop evaluation of the Rule  ")
+                return
+            }
 
             boolean res = rule.evaluate (facts, arg)
             ruleListeners.each {it?.afterEvaluate(rule, facts, res)}
@@ -32,19 +48,24 @@ class DefaultRuleEngine extends AbstractRuleEngine implements RuleEngine {
        assert rules, facts
 
        def prioritySortedRules = rules.sort {rule1, rule2 -> rule1.priority <=> rule2.priority}
-       Collection<Object> results = prioritySortedRules.iterator().collect { Rule rule ->
+       Collection<Object> resultsList = prioritySortedRules.iterator().collect { Rule rule ->
 
            def result
-           ruleListeners.each {it?.beforeExecute(rule, facts)}
-           try {
-               result = rule.execute (facts, arg)
-               ruleListeners.each {it?.onSuccess(rule, facts)}
-           } catch  (Exception e) {
-               ruleListeners.each { it?.onError (rule, facts, e) }
+           //check if any listeners.beforeExecute want to override the execution of the rule
+           if (check (facts, rule, arg)) {
+               ruleListeners.each { it?.beforeExecute(rule, facts) }
+               try {
+                   log.debug("run: executing rule $rule, passing facts <$facts> for preConditions check")
+                   result = rule.execute(facts, arg)
+                   ruleListeners.each { it?.onSuccess(rule, facts) }
+                   result
+               } catch (Exception e) {
+                   ruleListeners.each { it?.onError(rule, facts, e) }
+               }
            }
            result
        }
-       results
+       resultsList
    }
 
 
@@ -58,19 +79,31 @@ class DefaultRuleEngine extends AbstractRuleEngine implements RuleEngine {
     boolean check(Facts facts, Rule rule, arg = null) {
         assert rule, facts
 
-        ruleListeners.each {it?.beforeEvaluate(rule, facts)}
-        rule.evaluate(facts, arg)
-        ruleListeners.each {it?.afterEvaluate(rule, facts)}
+        def yehNey = ruleListeners.each {it?.beforeEvaluate(rule, facts)}
+        if (yehNey.any {it == false}) {
+            log.debug( "check facts with individual rule $rule: One of the RuleListeners returned false, stopping evaluation of the Rule  ")
+            return false
+        }
+
+        boolean result = rule.evaluate(facts, arg)
+        ruleListeners.each {it?.afterEvaluate(rule, facts, result)}
+        result
 
     }
 
     def run (Facts facts, Rule rule, arg = null) {
         assert rule, facts
 
+        def result
         ruleListeners.each {it?.beforeExecute(rule, facts)}
         try {
-            rule.execute (facts, arg)
-            ruleListeners.each {it?.onSuccess(rule, facts)}
+            //if check returns false - then the rule should not be run
+            if (check (facts, rule, arg)) {
+                log.debug ("run: execute the the rule with facts <$facts>, and arg  <$arg>")
+                result = rule.execute(facts, arg)
+                ruleListeners.each {it?.onSuccess(rule, facts)}
+            }
+            result
         } catch  (Exception e) {
             ruleListeners.each { it?.onError (rule, facts, e) }
         }
--- src/main/groovy/com/softwood/rules/scripts/testRules.groovy
+++ src/main/groovy/com/softwood/rules/scripts/testRules.groovy
@@ -3,16 +3,16 @@ package com.softwood.rules.scripts
 import com.softwood.rules.api.Condition
 import com.softwood.rules.api.Facts
 import com.softwood.rules.api.RuleEngine
+import com.softwood.rules.api.RuleFactory
 import com.softwood.rules.api.RuleListener
 import com.softwood.rules.api.RuleSet
 import com.softwood.rules.core.BasicAction
 import com.softwood.rules.core.DefaultRuleEngine
 import com.softwood.rules.core.BasicRule
 
-import java.beans.PropertyChangeListener
 
 def rule = new BasicRule (name:"rule#1", description: "first rule", priority: 1)
-//assign action to the rule
+//assign action to the rule, rule has no preconditions
 rule.action = new BasicAction (name:"action#1", description:"some action", action: {println "did rule action #1"; "act#1"})
 
 RuleSet rules = new RuleSet()
@@ -26,7 +26,10 @@ rules.register(rule)
 
 //create another rule this time with a precondition,
 def rule2 = new BasicRule (name:"rule#2", description: "second rule", priority: 0)
-rule2.action = new BasicAction (name:"act#2", description:"another action", action: {println "did rule action #2"; "act#2"})
+rule2.action = RuleFactory.newAction(name: "act#2", description: "rule2 action") {
+            println "did rule action #2";
+            "act#2"
+        }
 rule2.action.stateData << ["some state": "try this for size"]  //add some state data to the action
 println "rule2 now has state with ${rule2.action.stateData}"
 
@@ -51,29 +54,25 @@ Facts facts = new Facts(name:"wills facts", description:"starter for 10")
 //Fact fact= new DeprecateBasicFact (name:"sky", value:"isBlue")
 facts << ["sky": "isBlue"] <<  ["isWintery":true]
 
-//fact = new DeprecateBasicFact()
-//fact.setEntry(name:"sky", value:"isBlue")
-
 
 assert facts.size() == 2
 assert facts.name == "wills facts"
 assert facts.description == "starter for 10"
 
 //rule engine is statless so pass it any facts and any rules you want evaluated
-RuleEngine re = new DefaultRuleEngine()
+RuleEngine rulEngine = new DefaultRuleEngine()
 
-Map listener = [afterEvaluate: {lrule, lfacts, lresult -> println "listener: after evaluate rule $lrule with facts $lfacts returned $lresult "},
-                 beforeExecute: {lrule, lfacts -> println "listener: before the run the action on $lrule with facts $lfacts"},
-                 onSuccess: {lrule, lfacts -> println "listener: succeeded to run the action on $lrule with facts $lfacts"}
-                ]
-re.registerRuleListener( listener as RuleListener)
+Map listener = [
+        beforeEvaluate: {lrule, lfacts -> println "listener: before evaluate rule $lrule with facts $lfacts returning 'true' to continue "; true },
+        afterEvaluate: {lrule, lfacts, lresult -> println "listener: after evaluate rule $lrule with facts $lfacts returned $lresult "},
+        beforeExecute: {lrule, lfacts -> println "listener: before the run the action on $lrule with facts $lfacts"},
+        onSuccess: {lrule, lfacts -> println "listener: succeeded to run the action on $lrule with facts $lfacts"}
+        ]
 
-def res = re.run(facts, rules)
+rulEngine.registerRuleListener( listener as RuleListener)
+
+def res = rulEngine.run(facts, rules)
 
 println "results of check were : " + res
-//Iterable iFacts = facts.iterator()
-//println iFacts.size()
-
-//rule.evaluate([])
 
 
