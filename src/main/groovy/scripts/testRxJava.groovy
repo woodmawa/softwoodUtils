fcbe592821f1e2e4a14a35cb7e89c87044b407f0
woodmawa <will.woodman@btinternet.com>
2020-03-13 13:19:20 +0000

    fiddling with rxjava and closures

--- src/main/groovy/scripts/testRxJava.groovy
+++ src/main/groovy/scripts/testRxJava.groovy
@@ -5,35 +5,33 @@ import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.functions.Consumer
 import org.codehaus.groovy.runtime.MethodClosure
 
-import java.util.function.Function
-
 /**
  * class to wrap a closure and convert it into a RxJava Consumer
  * @param <T>  expected type of the arg that that the closure will be called with
  */
-class Consumable<T> implements Consumer<T> {
+class Processor<T> implements Consumer<T> {
     Closure action
     void accept (T arg) {
         action (arg)
     }
 
     static <T> Consumer<T>  from (Closure clos ) {
-        Consumable cons = new Consumable()
+        Processor cons = new Processor()
         cons.action = clos
         cons
     }
 
     static <T> MethodClosure asMethodClosure (Closure clos ) {
-        Consumable cons = new Consumable()
+        Processor cons = new Processor()
         cons.action = clos
         cons::accept
     }
 }
 
-Consumer cons = new Consumable (action : {num -> println num} )
-Consumer cons2 = Consumable.from {num -> println num}
+Consumer cons = new Processor (action : { num -> println num} )
+Consumer cons2 = Processor.from { num -> println num}
 
-MethodClosure mc = Consumable.asMethodClosure {println it}
+MethodClosure mc = Processor.asMethodClosure {println it}
 
 cons.accept(10)
 
