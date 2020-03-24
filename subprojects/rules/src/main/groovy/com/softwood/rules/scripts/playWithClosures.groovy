156ace83ae1cc26b0752573d5ceb4a9be199e427
woodmawa <will.woodman@btinternet.com>
2020-03-24 18:47:51 +0000

    lots of little fiddles

--- subprojects/rules/src/main/groovy/com/softwood/rules/scripts/playWithClosures.groovy
+++ subprojects/rules/src/main/groovy/com/softwood/rules/scripts/playWithClosures.groovy
@@ -1,20 +1,96 @@
 package com.softwood.rules.scripts
 
+import groovy.transform.InheritConstructors
 import org.codehaus.groovy.reflection.CachedClass
 import org.codehaus.groovy.reflection.CachedMethod
+import org.codehaus.groovy.runtime.InvokerHelper
 import org.codehaus.groovy.runtime.MethodClosure
 
-def clos = {println "hi "}
+Closure scriptClos = {println "within closure scriptClos this is $this, hi "}
 
-def methods  = clos.metaClass.getMethods()
+class A {
+    String name = "my A instance"
+    Closure aClos = {println " toby "}
+    def greeting() {
+        println "hi from my A"
+        "done"
+    }
+}
+aInst = new A()
 
+
+class ConcreteClosure extends Closure {
+
+
+    MethodClosure mc
+
+    ConcreteClosure(Object owner, Object thisObject, ctx) {
+        super(owner, thisObject)
+        mc = (Closure) owner::doCall
+        if (ctx){
+            mc.delegate = ctx
+            mc.resolveStrategy= Closure.DELEGATE_FIRST
+        }
+        assert mc
+    }
+
+    ConcreteClosure(Object owner, Object thisObject) {
+        super(owner, thisObject)
+        mc = (Closure) owner::doCall
+        assert mc
+    }
+
+    ConcreteClosure(Object owner) {
+        super(owner)
+        mc = (Closure) owner::doCall
+        assert mc
+
+    }
+
+    MethodClosure getMc() {
+        mc
+    }
+
+
+
+    def doCall() {
+        //println "call() called, invoke metaClass docall() " + InvokerHelper.invokeMethod(this, "doCall", null)
+        mc()
+
+    }
+}
+
+def constructorClos = {"return with, my concrete Closure, with this = $this, and owner = $owner, and name = ${name}"}
+constructorClos.delegate = aInst
+constructorClos.resolveStrategy= Closure.DELEGATE_FIRST
+//this will resolve a - if we set first, however if we set delegate inside Concrete closure it fails to resolve name!
+
+ConcreteClosure cc = new ConcreteClosure (constructorClos, constructorClos)
+
+println cc.call()
+
+MethodClosure aMc = aInst::greeting
+aMc()
+
+//scriptClos owned by instance of script, and aClos owner is instance a
+println "scriptclos owner : " + scriptClos.owner.toString() + " and aClos owner : " + aInst.aClos.owner.toString() + " a: ($aInst)"
+//scriptClos thisObject is instance of script, and aClos thisObject is instance a
+println "scriptclos thisObject : " + scriptClos.thisObject + " and aClos thisObject : " + aInst.aClos.thisObject.toString()
+
+
+def methods  = scriptClos.metaClass.getMethods().findAll {it.getName() =~ /doCall/}
+
+//methods for closure Does have a doCall() and doCall(Object)
+println "doCall method from methods "
 methods.each {println "$it"}
 
-def metaMethods = clos.metaClass.getMetaMethods()
+def metaMethods = scriptClos.metaClass.getMetaMethods().findAll { it.getName()  =~ /doCall/}
 
+//metaMethods for closure Doesnt have a doCall()
+println "doCall method from metaMethods "
+metaMethods.each {println "$it"}
 
-
-MetaMethod code = clos.metaClass.pickMethod("doCall", Object)
+MetaMethod code = scriptClos.metaClass.pickMethod("doCall", Object)
 //clos.metaClass.getMetaMethod("doCall", [])
 
 CachedClass clazz = code.getDeclaringClass()
@@ -23,14 +99,14 @@ CachedMethod cm = listCachedMethods[5]
 
 def out = System.out::println
 
-MethodClosure mr = clos::call
+MethodClosure mr = scriptClos::call
 
 MetaMethod codeClone = code.clone()
 
 assert code
 
 //chaneg to new closure
-clos = {println "William "}
+scriptClos = {println "William "}
 //codeClone.doMethodInvoke(clos, [])
 mr()
-clos()
+scriptClos()
