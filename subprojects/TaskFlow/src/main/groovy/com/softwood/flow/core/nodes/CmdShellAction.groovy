400900ffe06f2537dfb190cebf6c6dc18cce0049
woodmawa <will.woodman@btinternet.com>
2020-04-11 19:59:43 +0100

    added Classpathhelper to get files from resources directory

---
+++ subprojects/TaskFlow/src/main/groovy/com/softwood/flow/core/nodes/CmdShellAction.groovy
@@ -0,0 +1,199 @@
+package com.softwood.flow.core.nodes
+
+import com.softwood.flow.core.flows.FlowContext
+import com.softwood.flow.core.flows.FlowEvent
+import com.softwood.flow.core.flows.FlowType
+import groovy.util.logging.Slf4j
+import groovyx.gpars.dataflow.Promise
+
+import static groovyx.gpars.dataflow.Dataflow.task
+
+@Slf4j
+class CmdShellAction extends AbstractFlowNode {
+    def debug = false
+
+    static CmdShellAction newCmdShellAction (name = null, Closure closure) {
+        /*
+         *if we see an action declaration with closure, where the closure.owner is itself a closure, then check if the
+         * closure delegate is an Expando - if so we assume that this Expando is the ctx of a parenting flow
+         */
+        def ctx
+        def owner = closure.owner
+        def delegate = closure.delegate
+        if (owner instanceof Closure &&
+            delegate instanceof Closure &&
+            delegate?.delegate instanceof FlowContext) {
+            ctx = closure.delegate.delegate
+        } else {
+            ctx = FlowContext.newFreeStandingContext()
+        }
+
+        def ta = new CmdShellAction(ctx: ctx, name: name ?: "anonymous", action:closure)
+        ta.ctx?.taskActions << ta
+        if (ta.ctx?.flow)
+            ta.ctx?.flow.defaultSubflow.flowNodes << ta
+
+
+        if (ta.ctx.newInClosure != null)
+            ta.ctx.newInClosure << ta  //add to items generated within the running closure
+        ta
+
+    }
+
+    static CmdShellAction newCmdShellAction (name = null,  long delay, Closure closure) {
+        def ctx
+        def owner = closure.owner
+        def delegate = closure.delegate
+        if (owner instanceof Closure &&
+                delegate instanceof Closure &&
+                delegate?.delegate instanceof FlowContext) {
+            ctx = closure.delegate.delegate
+        } else {
+            ctx = FlowContext.newFreeStandingContext()
+        }
+
+        def ta = new CmdShellAction(ctx: ctx, taskDelay: delay, name: name ?: "anonymous", action:closure)
+        ta.ctx?.taskActions << ta
+        if (ta.ctx?.flow)
+            ta.ctx?.flow.defaultSubflow.flowNodes << ta
+        if (ta.ctx.newInClosure != null)
+            ta.ctx.newInClosure << ta  //add to items generated within the running closure
+
+        ta
+
+    }
+
+
+    /**
+     * run the closure as task, by invoking private doRun().  result holds the answer as a DF promise
+     * @param args - optional Object[]
+     * @param errHandler - closure to call in case of exception being triggered
+     * @return 'this' FlowNode
+     */
+    def run (Object[] args, Closure errHandler = null) {
+        doRun (null, args, errHandler)
+    }
+
+    def run (Closure errHandler = null) {
+        doRun (null, null, errHandler)
+    }
+
+    def run (AbstractFlowNode previousNode, Object[] args = null, Closure errHandler = null) {
+        doRun (previousNode, args, errHandler)
+    }
+    def delayedRun (int delay, args = null, Closure errHandler = null) {
+        taskDelay = delay
+        run (args, errHandler)
+    }
+
+    /**
+     * internal routine to execute the closure as a gpars task, and places the promise into field 'result'
+     * also sets onBound closure to remove the promise from the ctx.activePromises list.
+     *
+     * if passed an error handler will invoke that and set the flowNode status to 'errors' and returns the flow node
+     *
+     * @param errHandler
+     * @return
+     */
+    private def doRun  (AbstractFlowNode previousNode, args = null, Closure errHandler = null) {
+        //println " doRun: made it to action task with previousTask $previousNode, and args as $args, and errHandler $errHandler"
+        if (taskDelay == 0)
+            status = FlowNodeStatus.running
+        else
+            status = FlowNodeStatus.deferred
+
+
+        def ta = actionTask (previousNode, this, args, errHandler)
+        ta
+    }
+
+    private def actionTask (CmdShellAction previousNode, AbstractFlowNode step,  Object[] args, Closure errHandler = null) {
+        try {
+
+            def cloned  = step.action.clone()
+            cloned.delegate = step.ctx
+            cloned.resolveStrategy = Closure.DELEGATE_FIRST
+
+
+            //if deferred in time, sleep required period of ms
+            if (taskDelay) {
+                Thread.sleep( taskDelay, {println "thread interupted "})
+                log.debug "ActionTask: finished delay $taskDelay ms, now scheduling task"
+            }
+
+            step.ctx?.flowListeners.each { listener ->
+                listener.beforeFlowNodeExecuteState (step.ctx, this)
+            }
+            //schedule task and receive the future and store it
+            //pass promise from this into new closure in the task
+
+            //args can get padded with null arg at the end of the list.  So if see the null, then strip it off
+            def size = args.size()
+            if (args?[size-1] == null)
+                args = args.toList().subList(0, size-1)
+
+            Promise promise  = task {
+                ProcessBuilder processBldr = new ProcessBuilder ("cmd", "/c", *args)
+                def process = processBldr.start()
+                def ans = process.text
+                int exitCode = process.waitFor();
+                //todo throw exception if we see error code ?
+
+                exitCode == 0 ? ans : exitCode
+
+            }
+            step.result = promise
+            step.ctx.activePromises << promise
+            //when DF is bound remove promise from ctx.activePromises
+            promise.whenBound {
+                status = FlowNodeStatus.completed
+                boolean yesNo = step.ctx?.activePromises.remove (promise)
+                assert yesNo
+
+                step.ctx?.flowListeners.each { listener ->
+                    listener.afterFlowNodeExecuteState (ctx, this)
+                    if(ctx.type = FlowType.Process) {
+                        FlowEvent fe = new FlowEvent<>(flow: ctx.flow,  message: "completed task #$sequence with '$name' ", referencedObject: this)
+                        listener.flowEventUpdate(ctx, fe)
+                    }
+                }
+
+                log.debug "CmdShell actionTask(): promise was bound, removed the promise from activePromises: $yesNo, and activePromises : " + ctx?.activePromises
+
+            }
+
+            step
+        } catch (Exception e) {
+            if (errHandler) {
+                log.debug "doRun()  hit exception $e"
+                status = FlowNodeStatus.errors
+                errHandler(e, this)
+            }
+            step
+        }
+
+    }
+
+    def  then (AbstractFlowNode nextStep, args = null, Closure errHandler = null) {
+        assert nextStep, "was expecting non null nextStep"
+
+        //todo replace with log
+        log.debug "then : fire <$nextStep.name> action "
+
+        Object[] EMPTY_ARGS = []
+
+        super.then (nextStep, errHandler)
+
+        def previousTask = this
+        actionTask (previousTask, nextStep, args ?: EMPTY_ARGS)
+
+    }
+
+    String toString () {
+        if (debug == false)
+            "CmdShellAction (name:$name, status:$status)"
+        else
+            "CmdShellAction (name:$name, status:$status, action:${action.toString()})"
+    }
+}
+
