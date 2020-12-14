66665e41ae55c681fd41f5a2443326577ba76bcf
woodmawa <will.woodman@btinternet.com>
2020-12-14 16:03:50 +0000

    added start of ms office project - starting with basic Excel class

---
+++ subprojects/Office/src/main/groovy/com/softwood/office/Excel.groovy
@@ -0,0 +1,119 @@
+package com.softwood.office
+
+
+import org.apache.poi.ss.usermodel.*
+import org.apache.poi.hssf.usermodel.*
+import org.apache.poi.xssf.usermodel.*
+import org.apache.poi.ss.util.*
+import org.apache.poi.ss.usermodel.*
+import java.io.*
+
+//http://poi.apache.org/spreadsheet/quick-guide.html#Iterator
+class Excel {
+
+    def parse(String path, sheetIndex=0) {
+        if (!path.contains(".xlsx")) {
+            path.concat(".xlsx")
+        }
+
+        InputStream inp = new FileInputStream(path)
+        Workbook wb = WorkbookFactory.create(inp)
+        Sheet sheet = wb.getSheetAt(sheetIndex)
+
+        Iterator<Row> rowIt = sheet.rowIterator()
+        Row row = rowIt.next()
+        def headers = getRowData(row)
+
+        def rows = []
+        while(rowIt.hasNext()) {
+            row = rowIt.next()
+            rows << getRowData(row)
+        }
+        [headers, rows]
+    }
+
+    def getRowData(Row row) {
+        def data = []
+        for (Cell cell : row) {
+            getValue(row, cell, data)
+        }
+        data
+    }
+
+    def getRowReference(Row row, Cell cell) {
+        def rowIndex = row.getRowNum()
+        def colIndex = cell.getColumnIndex()
+        CellReference ref = new CellReference(rowIndex, colIndex)
+        ref.formatAsString()
+        /* switch (cell.getCellType()) {
+            case CellType.STRING:
+                cell.getRichStringCellValue().getString()
+                break
+
+            case CellType.NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    cell.getDateCellValue()
+                } else {
+                    cell.getNumericCellValue()
+                }
+                break
+
+            case CellType.BOOLEAN:
+                cell.getBooleanCellValue()
+                break
+
+            case CellType.FORMULA:
+                cell.getCellFormula()
+                break
+
+            case CellType.BLANK:
+                "BLANK"
+                break
+        }
+        ref.getRichStringCellValue().getString()
+        */
+
+    }
+
+    def getValue(Row row, Cell cell, List data) {
+        def rowIndex = row.getRowNum()
+        def colIndex = cell.getColumnIndex()
+        def value = ""
+        switch (cell.getCellType()) {
+            case CellType.STRING:
+                value = cell.getRichStringCellValue().getString();
+                break
+
+            case CellType.NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    value = cell.getDateCellValue()
+                } else {
+                    value = cell.getNumericCellValue()
+                }
+                break
+
+            case CellType.BOOLEAN:
+                value = cell.getBooleanCellValue()
+                break
+
+            case CellType.FORMULA:
+                value = cell.getCellFormula()
+                break
+
+            default:
+                value = ""
+        }
+        data[colIndex] = value
+        data
+    }
+
+    def toXml(header, row) {
+        def obj = "<object>\n"
+        row.eachWithIndex { datum, i ->
+            def headerName = header[i]
+            obj += "\t<$headerName>$datum</$headerName>\n"
+        }
+        obj += "</object>"
+    }
+
+}
---
+++ subprojects/Office/src/main/groovy/com/softwood/scripts/testExcel.groovy
@@ -0,0 +1,20 @@
+package com.softwood.scripts
+
+import com.softwood.office.Excel
+
+Excel excel = new Excel()
+
+def (headers, rows) = excel.parse("D:\\Tableau play data\\nov sales data.xlsx")
+
+println 'Headers'
+println '------------------'
+headers.each { header ->
+    println header
+}
+
+println "\n"
+println 'Rows'
+println '------------------'
+rows.each { row ->
+    println excel.toXml(headers, row)
+}
